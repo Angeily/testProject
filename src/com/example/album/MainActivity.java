00160aa980385bcb315fd81ff5cf79a4580e73d7
Mr.yang <1040069348@qq.com>
2016-12-17 21:19:27 +0800

    增加按键点击效果

--- src/com/example/album/MainActivity.java
+++ src/com/example/album/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.album;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import android.R.color;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -24,6 +29,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,161 +39,194 @@ import android.os.Build;
 public class MainActivity extends Activity {
 
 	private TextView text1;
-	private Button button1,button2;
+	private Button button1, button2;
 	private ImageView imageView;
 	private ActionBar actionBar;
-	private boolean keyStop = false,fullScreen = true;
+	private boolean keyStop = false, fullScreen = true;
 	private String version;
-	private boolean isAndroidM,noEvent;
+	private boolean isAndroidM, noEvent;
 	private static final String TAG = "MainActivity";
 	private static final int MSG_START_DREAM = 10;
 	private static final int MSG_ACTIONBAR_SHOW = 11;
-	
-	private OnClickListener listener = new OnClickListener(){
-		
+
+	private OnClickListener listener = new OnClickListener() {
+
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			
-			switch(v.getId()) {
+
+			switch (v.getId()) {
 			case R.id.button_1:
-				Intent intent = new Intent(Intent.ACTION_GET_CONTENT,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-	            startActivityForResult(intent, 0);
+				Intent intent = new Intent(
+						Intent.ACTION_GET_CONTENT,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, 0);
 				break;
 			case R.id.button_2:
 				break;
-			
+
 			default:
-				
+
 				break;
 			}
 		}
 	};
 	
-	OnFocusChangeListener focusListener = new OnFocusChangeListener() {
+	private OnTouchListener touchListener = new OnTouchListener() {
+		
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.button_1:
+				if(event.getAction()==MotionEvent.ACTION_DOWN){  
+	                button1.setBackgroundResource(R.drawable.button_focus);  
+	            }else if(event.getAction()==MotionEvent.ACTION_UP){  
+	                button1.setBackgroundResource(R.drawable.button_background);  
+	            }  
+				break;
+			case R.id.button_2:
+				if(event.getAction()==MotionEvent.ACTION_DOWN){  
+	                button2.setBackgroundResource(R.drawable.button_focus);  
+	            }else if(event.getAction()==MotionEvent.ACTION_UP){  
+	                button2.setBackgroundResource(R.drawable.button_background);  
+	            }  
+				break;
+			default:
+
+				break;
+			}
+			return false;
+		}
+	};
+
+	private OnFocusChangeListener focusListener = new OnFocusChangeListener() {
 
 		@Override
 		public void onFocusChange(View v, boolean hasFocus) {
 			// TODO Auto-generated method stub
-			switch(v.getId()) {
-			case R.id.button_1: 
-				if(hasFocus) {
+			switch (v.getId()) {
+			case R.id.button_1:
+				if (hasFocus) {
 					button1.setBackgroundResource(R.drawable.button_focus);
-				}else {
-					button1.setBackgroundColor(color.white);
+				} else {
+					button1.setBackgroundResource(R.drawable.button_background);
 				}
 				break;
 			case R.id.button_2:
-				if(hasFocus) {
+				if (hasFocus) {
 					button2.setBackgroundResource(R.drawable.button_focus);
-				}else {
-					button2.setBackgroundColor(color.white);
+				} else {
+					button2.setBackgroundResource(R.drawable.button_background);
 				}
 			}
-			
+
 		}
-		
+
 	};
-	
+
 	@SuppressLint("HandlerLeak")
 	private Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
-			switch(msg.what) {
-			case MSG_START_DREAM:  //dreaming need to wait keyevent pass by
-				Log.d(TAG,"handle the message : " + msg.what);
-				if(isAndroidM) {
+			switch (msg.what) {
+			case MSG_START_DREAM: // dreaming need to wait keyevent pass by
+				Log.d(TAG, "handle the message : " + msg.what);
+				if (isAndroidM) {
 					startDream();
-				}else {
-					Toast.makeText(MainActivity.this, "系统不支持", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(MainActivity.this, "系统不支持",
+							Toast.LENGTH_SHORT).show();
 				}
 				break;
 			case MSG_ACTIONBAR_SHOW:
-				/*if(noEvent) {
-					if(!fullScreen) {
-						actionBar.hide();
-					}
-				}*/
+				/*
+				 * if(noEvent) { if(!fullScreen) { actionBar.hide(); } }
+				 */
 				noEvent = true;
-				//handler.sendEmptyMessageDelayed(MSG_ACTIONBAR_SHOW, 10000);
+				// handler.sendEmptyMessageDelayed(MSG_ACTIONBAR_SHOW, 10000);
 				break;
 			default:
-				Log.d(TAG,"msg : " + msg.what);
+				Log.d(TAG, "msg : " + msg.what);
 				break;
 			}
 		}
 	};
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		button1 = (Button) findViewById(R.id.button_1);
 		button2 = (Button) findViewById(R.id.button_2);
-		text1 = (TextView)findViewById(R.id.textview_1);
-		imageView = (ImageView)findViewById(R.id.image_view_1);
-		Log.d(TAG,"oncreate");
-		button1.setOnClickListener(listener); 
+		text1 = (TextView) findViewById(R.id.textview_1);
+		imageView = (ImageView) findViewById(R.id.image_view_1);
+		Log.d(TAG, "oncreate");
+		button1.setOnClickListener(listener);
 		button2.setOnClickListener(listener);
 		button1.setOnFocusChangeListener(focusListener);
 		button2.setOnFocusChangeListener(focusListener);
-		imageView = (ImageView)findViewById(R.id.image_view_1);
+		button1.setOnTouchListener(touchListener);
+		button2.setOnTouchListener(touchListener);
+		imageView = (ImageView) findViewById(R.id.image_view_1);
 		imageView.setVisibility(View.INVISIBLE);
 		actionBar = getActionBar();
 		version = Build.VERSION.RELEASE;
 		isAndroidM = isAndroidM();
-		if(fullScreen) {
+		if (fullScreen) {
 			actionBar.hide();
 		}
-		//handler.sendEmptyMessageDelayed(MSG_ACTIONBAR_SHOW,10000);
+		// handler.sendEmptyMessageDelayed(MSG_ACTIONBAR_SHOW,10000);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
-		Log.d(TAG,"onDestroy");
+		Log.d(TAG, "onDestroy");
 		super.onDestroy();
 	};
-	
+
 	@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==RESULT_CANCELED) {
-            return;
-        }
-        if (requestCode==0) {
-            Uri uri = data.getData();
-            bitmapFactory(uri);
-        }
-    }
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_CANCELED) {
+			return;
+		}
+		if (requestCode == 0) {
+			Uri uri = data.getData();
+			bitmapFactory(uri);
+		}
+	}
 
 	private void bitmapFactory(Uri uri) {
 		// TODO Auto-generated method stub
 		BitmapFactory.Options options = new BitmapFactory.Options();
-        //options.inSampleSize = 8;//设置生成的图片为原图的八分之一
-        options.inJustDecodeBounds = true;//这将通知 BitmapFactory类只须返回该图像的范围,而无须尝试解码图像本身
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        int heightRadio = (int) Math.ceil(options.outHeight/(float)height);
-        int widthRadio = (int) Math.ceil(options.outWidth/(float)width);
-        if (heightRadio>1&&widthRadio>1) {
-            if (heightRadio>widthRadio) {
-                options.inSampleSize = heightRadio;
-            }else {
-                options.inSampleSize = widthRadio;
-            }
-        }
-        //真正解码图片
-        options.inJustDecodeBounds = false;
-        Bitmap b;
-        try {
-            b = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri),null, options);
-            imageView.setVisibility(View.VISIBLE);
-            imageView.setImageBitmap(b);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-		
+		// options.inSampleSize = 8;//设置生成的图片为原图的八分之一
+		options.inJustDecodeBounds = true;// 这将通知
+											// BitmapFactory类只须返回该图像的范围,而无须尝试解码图像本身
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int width = dm.widthPixels;
+		int height = dm.heightPixels;
+		int heightRadio = (int) Math.ceil(options.outHeight / (float) height);
+		int widthRadio = (int) Math.ceil(options.outWidth / (float) width);
+		if (heightRadio > 1 && widthRadio > 1) {
+			if (heightRadio > widthRadio) {
+				options.inSampleSize = heightRadio;
+			} else {
+				options.inSampleSize = widthRadio;
+			}
+		}
+		// 真正解码图片
+		options.inJustDecodeBounds = false;
+		Bitmap b;
+		try {
+			b = BitmapFactory.decodeStream(getContentResolver()
+					.openInputStream(uri), null, options);
+			imageView.setVisibility(View.VISIBLE);
+			imageView.setImageBitmap(b);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -203,14 +242,15 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		switch(id) {
+		switch (id) {
 		case R.id.action_settings:
 			break;
 		case R.id.menu_startgame:
-			if(isAndroidM) {
+			if (isAndroidM) {
 				startGame();
-			}else {
-				Toast.makeText(MainActivity.this, "系统不支持该游戏", Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(MainActivity.this, "系统不支持该游戏",
+						Toast.LENGTH_SHORT).show();
 			}
 		default:
 			break;
@@ -220,46 +260,49 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
+	public boolean dispatchKeyEvent(KeyEvent event) {
 		int keycode = event.getKeyCode();
 		switch (keycode) {
 		case KeyEvent.KEYCODE_MENU:
-			if(event.getAction() == KeyEvent.ACTION_UP){
-				if(isAndroidM) {
+			if (event.getAction() == KeyEvent.ACTION_UP) {
+				if (isAndroidM) {
 					showMenu();
 				}
 			}
 			break;
 		case KeyEvent.KEYCODE_PROG_YELLOW:
-			if(event.getAction() == KeyEvent.ACTION_UP){
-				if(isAndroidM) {
+			if (event.getAction() == KeyEvent.ACTION_UP) {
+				if (isAndroidM) {
 					startGame();
-				}else {
-					Toast.makeText(MainActivity.this, "系统不支持该游戏", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(MainActivity.this, "系统不支持该游戏",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 			break;
-		case KeyEvent.KEYCODE_PROG_RED: //red key to prevent other key hit 
-			if(event.getAction() == KeyEvent.ACTION_UP){
+		case KeyEvent.KEYCODE_PROG_RED: // red key to prevent other key hit
+			if (event.getAction() == KeyEvent.ACTION_UP) {
 				keyStop = !keyStop;
-				if(keyStop) {
-					Toast.makeText(MainActivity.this, "大部分按键被屏蔽！", Toast.LENGTH_SHORT).show();
-				}else {
-					Toast.makeText(MainActivity.this, "屏蔽解除！", Toast.LENGTH_SHORT).show();
+				if (keyStop) {
+					Toast.makeText(MainActivity.this, "大部分按键被屏蔽！",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(MainActivity.this, "屏蔽解除！",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 			break;
-		case KeyEvent.KEYCODE_PROG_BLUE://blue key to start dreaming
-			if(event.getAction() == KeyEvent.ACTION_UP){
-				handler.sendEmptyMessageDelayed(MSG_START_DREAM,500);		
-			}			
+		case KeyEvent.KEYCODE_PROG_BLUE:// blue key to start dreaming
+			if (event.getAction() == KeyEvent.ACTION_UP) {
+				handler.sendEmptyMessageDelayed(MSG_START_DREAM, 500);
+			}
 			break;
-		case KeyEvent.KEYCODE_PROG_GREEN:	
-			if(event.getAction() == KeyEvent.ACTION_UP){
-				if(fullScreen) {
+		case KeyEvent.KEYCODE_PROG_GREEN:
+			if (event.getAction() == KeyEvent.ACTION_UP) {
+				if (fullScreen) {
 					actionBar.show();
 					fullScreen = false;
-				}else {
+				} else {
 					actionBar.hide();
 					fullScreen = true;
 				}
@@ -267,59 +310,109 @@ public class MainActivity extends Activity {
 			break;
 		default:
 			break;
-		}	
-		if(keyStop) {
+		}
+		if (keyStop) {
 			return true;
-		}else {
+		} else {
 			noEvent = false;
-			/*if(!fullScreen) {
-				actionBar.show();
-			}*/
+			/*
+			 * if(!fullScreen) { actionBar.show(); }
+			 */
 			return super.dispatchKeyEvent(event);
 		}
-		
+
 	}
-	
+
 	@Override
 	public boolean dispatchTouchEvent(MotionEvent ev) {
+		
 		super.dispatchTouchEvent(ev);
 		noEvent = false;
-		/*if(!fullScreen) {
-			actionBar.show();
-		}*/
+		/*
+		 * if(!fullScreen) { actionBar.show(); }
+		 */
 		return true;
 	}
-	
+
 	public void showMenu() {
 		View mDecor = getWindow().getDecorView();
-        int vgbId;
-        try {
-            Class docor = Class.forName ( "com.android.internal.widget.ActionBarOverlayLayout" );
-            Class c = Class.forName ( "com.android.internal.R$id" );
-            Object obj = c.newInstance();
-            Field field = c.getField ( "decor_content_parent" );
-            vgbId = field.getInt ( obj );
-            Object vgb =  mDecor.findViewById ( vgbId );
-            Method show = docor.getMethod("showOverflowMenu");
-            show.invoke(vgb);
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
+		int vgbId;
+		try {
+			Class docor = Class
+					.forName("com.android.internal.widget.ActionBarOverlayLayout");
+			Class c = Class.forName("com.android.internal.R$id");
+			Object obj = c.newInstance();
+			Field field = c.getField("decor_content_parent");
+			vgbId = field.getInt(obj);
+			Object vgb = mDecor.findViewById(vgbId);
+			Method show = docor.getMethod("showOverflowMenu");
+			show.invoke(vgb);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void startDream() {
 		Intent intent = new Intent();
-		intent.setClassName("com.android.deskclock", "com.android.deskclock.ScreensaverActivity");
+		intent.setClassName("com.android.deskclock",
+				"com.android.deskclock.ScreensaverActivity");
 		startActivity(intent);
 	}
-	
+
 	public void startGame() {
 		Intent intent = new Intent();
-		intent.setClassName("com.android.systemui", "com.android.systemui.egg.MLandActivity");
+		intent.setClassName("com.android.systemui",
+				"com.android.systemui.egg.MLandActivity");
 		startActivity(intent);
 	}
-	
-	public boolean isAndroidM() {		
+
+	public boolean isAndroidM() {
 		return (version.contains("6.0"));
 	}
+	public void clean(){
+	ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);  
+    List<RunningAppProcessInfo> infoList = am.getRunningAppProcesses();  
+    List<ActivityManager.RunningServiceInfo> serviceInfos = am.getRunningServices(100);  
+
+    long beforeMem = getAvailMemory(this);  
+    Log.d(TAG, "-----------before memory info : " + beforeMem);  
+    int count = 0;  
+    if (infoList != null) {  
+        for (int i = 0; i < infoList.size(); ++i) {  
+            RunningAppProcessInfo appProcessInfo = infoList.get(i);  
+            Log.d(TAG, "process name : " + appProcessInfo.processName);  
+            //importance 该进程的重要程度  分为几个级别，数值越低就越重要。  
+            Log.d(TAG, "importance : " + appProcessInfo.importance);  
+
+            // 一般数值大于RunningAppProcessInfo.IMPORTANCE_SERVICE的进程都长时间没用或者空进程了  
+            // 一般数值大于RunningAppProcessInfo.IMPORTANCE_VISIBLE的进程都是非可见进程，也就是在后台运行着  
+            if (appProcessInfo.importance > RunningAppProcessInfo.IMPORTANCE_VISIBLE) {  
+                String[] pkgList = appProcessInfo.pkgList;  
+                for (int j = 0; j < pkgList.length; ++j) {//pkgList 得到该进程下运行的包名  
+                    Log.d(TAG, "It will be killed, package name : " + pkgList[j]);  
+                    am.killBackgroundProcesses(pkgList[j]);  
+                    count++;  
+                }  
+            }  
+
+        }  
+    }  
+
+    long afterMem = getAvailMemory(this);  
+    Log.d(TAG, "----------- after memory info : " + afterMem);  
+    Toast.makeText(MainActivity.this, "clear " + count + " process, "  
+                + (afterMem - beforeMem) + "M", Toast.LENGTH_LONG).show();  
+	}
+	public long getAvailMemory(Context context) {  
+        // 获取android当前可用内存大小  
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);  
+        MemoryInfo mi = new MemoryInfo();  
+        am.getMemoryInfo(mi);  
+        //mi.availMem; 当前系统的可用内存  
+        //return Formatter.formatFileSize(context, mi.availMem);// 将获取的内存大小规格化  
+        Log.d(TAG, "可用内存---->>>" + mi.availMem / (1024 * 1024));  
+        return mi.availMem / (1024 * 1024);  
+    }  
 }
+
+
