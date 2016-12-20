b85780875fd2a3907bc28bc0fb3938e7b869b85b
giec <1040069348@qq.com>
2016-12-20 20:50:10 +0800

    add some detail about music page

--- src/com/example/album/MusicActivity.java
+++ src/com/example/album/MusicActivity.java
@@ -26,45 +26,13 @@ public class MusicActivity extends Activity {
         /** 匀速插值器 */  
         LinearInterpolator lir = new LinearInterpolator();  
         anim.setInterpolator(lir);  
-        anim.setDuration(9000);
+        anim.setDuration(15000);
         anim.setRepeatCount(-1);
 		//Animation testAnim = AnimationUtils.loadAnimation(this, R.anim.anim_music);  
 		imageView1.startAnimation(anim);  
 		
 	}
 	
-    public void positive(View v) {  
-        Animation anim = new RotateAnimation(1, 360 , Animation.RELATIVE_TO_SELF, 0.5f,  
-                Animation.RELATIVE_TO_SELF, 0.5f);  
-        /** 匀速插值器 */  
-        LinearInterpolator lir = new LinearInterpolator();  
-        anim.setInterpolator(lir);  
-        anim.setDuration(3000);
-        anim.setRepeatCount(-1);
-        /** 动画完成后不恢复原状 */  
-        //anim.setFillAfter(true);  
-      /*  currAngle += 180;  
-        if (currAngle > 360) {  
-            currAngle = currAngle - 360;  
-        }  */
-        imageView1.startAnimation(anim);  
-    }  
-      
-    public void negative(View v) {  
-    	  Animation anim = new RotateAnimation(0, -360 , Animation.RELATIVE_TO_SELF, 0.5f,  
-                  Animation.RELATIVE_TO_SELF, 0.5f);  
-          /** 匀速插值器 */  
-          LinearInterpolator lir = new LinearInterpolator();  
-          anim.setInterpolator(lir);  
-          anim.setDuration(3000);
-          anim.setRepeatCount(Animation.INFINITE);
-          /** 动画完成后不恢复原状 */  
-          anim.setFillAfter(true);  
-        /*  currAngle += 180;  
-          if (currAngle > 360) {  
-              currAngle = currAngle - 360;  
-          }  */
-          imageView1.startAnimation(anim);
-    }
+
 
 }
